4807a1bf884273a8936f9634e667b8e963766753
FiffyD <fiffydaphnyee@gmail.com>
2026-08-12 12:18:45 +0800

    change in lab 6

---
+++ labs/lab06/lab-test-2.py
@@ -0,0 +1,11 @@
+#Input Student information
+Student = "\nName: Ahmad Bin Abu    \t\t\t  Matric. No: MS2025123499"
+#Input the escape characters to produce output
+Input = "\n\n*\t\t*\n**\t   **\n***   ***\n*******\n***   ***\n**\t   **\n*\t\t*"
+#Input message and calculate total mark 
+message = "\n\n\nThis is my\n\tsecond\n\t      assignment"
+mark =2*10
+message2 = "\nI want 2x10 marks, which is" 
+message3 ="full marks"
+#Display output message and print
+print(Student,Input,message,message2,mark,message3)
