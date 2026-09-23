0559c498d223da004477af970e45193bb8d7ea4c
FiffyD <fiffydaphnyee@gmail.com>
2026-09-23 12:32:50 +0800

    change

--- labs/lab08/exercise4/exercise4.py
+++ labs/lab08/exercise4/exercise4.py
@@ -1,7 +1,19 @@
 current_reading = int(input())
 previous_reading = int(input())
 
+consumption = current_reading - previous_reading 
 
+if consumption <= 20 :
+    water_cost = consumption * 0.57
+elif consumption <= 35 :
+    water_cost = (0.57* 20) + ((consumption - 20) * 1.03)
+else :
+    water_cost = (0.57*20) + (1.03*15) + ((consumption - 35) * 1.40 )
+
+service_charge = 8
+sewerage = 2
+extra_charges = service_charge + sewerage
+total_bill = water_cost + extra_charges 
 
 print(consumption)
 print(water_cost)
--- labs/lab08/exercise5/exercise5.py
+++ labs/lab08/exercise5/exercise5.py
@@ -2,6 +2,24 @@ main_course = input()
 drink = input()
 dessert = input()
 
+if main_course == "Chicken" :
+    course_price = 10
+elif main_course == "Beef" :
+    course_price = 12
+else :
+    price = 11
 
+if drink == "Soft Drink" :
+    price = 2
+else :
+    price = 3
+
+if dessert == "Ice Cream" :
+    price = 4
+else :
+    price = 5
+
+menu_prices = main_course + drink + dessert
+final_bill = menu_prices + (menu_prices * 0.10)
 
 print(f"{final_bill:.2f}")
